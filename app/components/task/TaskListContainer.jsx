74077a6e97d4458974959e608c09c74fab8bd063
shawnxusy <shawn.xusy@gmail.com>
2016-01-18 22:25:38 -0800

    integrated highcharts

--- app/components/task/TaskListContainer.jsx
+++ app/components/task/TaskListContainer.jsx
@@ -1,8 +1,16 @@
 /* jshint esnext:true */
 
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 import TaskListStore from 'stores/task/TaskListStore';
 import TaskListActions from 'actions/task/TaskListActions';
+
+const isBrowser = typeof window !== 'undefined';
+if (isBrowser) {
+  var Highcharts = require('react-highcharts/dist/bundle/highcharts');
+  var ReactHighcharts = require('react-highcharts');
+}
 
 export default class TaskListContainer extends React.Component {
   constructor(props) {
@@ -14,6 +22,8 @@ export default class TaskListContainer extends React.Component {
   componentDidMount() {
     TaskListStore.listen(this.onChange);
     TaskListActions.getTasks();
+
+    ReactDOM.render(React.createElement(ReactHighcharts, { config: this.state.config }), document.getElementById('test'));
   }
 
   componentWillUnmount() {
@@ -35,6 +45,8 @@ export default class TaskListContainer extends React.Component {
 
     return (
       <div>
+        <div id="test">
+        </div>
         <h3>A list of tasks listed here</h3>
         {taskNodes}
       </div>
